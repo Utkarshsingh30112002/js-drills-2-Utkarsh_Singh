70ee6c2f0191ed2f5879d12b766c81f7410b4517
Utkarsh Singh <utkarshsingh30112002@gmail.com>
2024-09-04 23:06:35 +0530

    handeled corner cases

--- solution.js
+++ solution.js
@@ -1,5 +1,42 @@
+function validArgument(users){
+    if(!users){
+        console.log("Pass a valid argument");
+        return false;
+    }
+    let arr=Object.keys(users);
+    if(users.length==0){
+        console.log("Pass a valid argument");
+        return false;
+    }
+    for(let i=0;i<arr.length;i++){
+        if(!Number.isInteger(users[arr[i]].age)||users[arr[i]].age<0){
+            console.log(`Check age of ${arr[i]}`);
+            return false;
+        }
+        if(!users[arr[i]].desgination||users[arr[i]].desgination.trim().length==0){
+            console.log("Check desgination of "+arr[i]);
+            return false;
+        }
+        if(!users[arr[i]].qualification||users[arr[i]].qualification.trim().length==0){
+            console.log("Check qualification of "+arr[i]);
+            return false;
+        }
+        if(!users[arr[i]].nationality||users[arr[i]].nationality.trim().length==0){
+            console.log("Check nationality of "+arr[i]);
+            return false;
+        }
+        if(!Array.isArray(users[arr[i]].interests)||users[arr[i]].interests.length==0){
+            console.log("Check interests of "+arr[i]);
+            return false;
+        }
+    }
+    return true;
+}
+//Wrote the valid argument which checks the formet of users object other wise it gives error message and undefined
+
 // Q1 Find all users who are interested in playing video games.
 function intrestInVideoGames(users){
+    if(!validArgument(users))return;
     let reg=/video games/ig;
     let arr=[];
     for(let user in users){
@@ -9,6 +46,7 @@ function intrestInVideoGames(users){
 }
 // Q2 Find all users staying in Germany.
 function inGermany(users){
+    if(!validArgument(users))return;
     let arr=[];
     for(let user in users){
         if(users[user].nationality=="Germany")arr.push(user);
@@ -17,6 +55,7 @@ function inGermany(users){
 }
 // Q3 Find all users with masters Degree.
 function withMasters(users){
+    if(!validArgument(users))return;
     let arr=[];
     for(let user in users){
         if(users[user].qualification=="Masters")arr.push(user);
@@ -25,6 +64,7 @@ function withMasters(users){
 }
 // Q4 Group users based on their Programming language mentioned in their designation.
 function groupUsers(users){
+    if(!validArgument(users))return;
     let regPython=/python/i;
     let regGolang=/golang/i;
     let regJavascript=/javascript/i;
--- test.js
+++ test.js
@@ -45,19 +45,26 @@ const users = {
 const ans = intrestInVideoGames(users);
 //  intrestInVideoGames();
 //  intrestInVideoGames();
- console.log(ans)
+ console.log("These users are intrested in playing video games :-");
+ console.log(ans);
 // // Q2 Find all users staying in Germany.
+console.log("These are the users Staying in Germany :-");
  console.log(inGermany(users));
 // inGermany();
 // inGermany();
  
 // // Q3 Find all users with masters Degree.
+console.log("These are the users having masters Degree :-");
  console.log(withMasters(users));
 // withMasters();
 // withMasters();
 
 // // Q4 Group users based on their Programming language mentioned in their designation.
+console.log("Users based on programming language :-");
   console.log(groupUsers(users));
+  console.log(groupUsers([]));
+  console.log(groupUsers({a:{}}));
+  console.log(groupUsers());
 //  groupUsers();
 //  groupUsers();
 
